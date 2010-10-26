d21d2080fdcab822b7550e6bc6800ab6c7976444
Robert Crossfield <robcrossfield@gmail.com>
2010-10-26 08:41:12 +0000

    Add last main game elements, altho there is a problem with the teleporter

---
+++ head/src/castle/objects/objectFrankenstein.hpp
@@ -0,0 +1,33 @@
+
+class cRoom;
+
+class cObjectFrankenstein : public cObject {
+public:
+	byte	mState;
+
+public:
+	cObjectFrankenstein( cRoom *pRoom, byte pPosX, byte pPosY ) : cObject( pRoom, pPosX, pPosY ) {
+		mObjectID = eObjectFrankenstein;
+		mDrags = false;
+		mLength = 1;
+		mParts = false;
+		mState = 0;
+	}
+
+	size_t		objectLoad( byte **pBuffer ) {
+		
+		return 0;
+	}
+
+	size_t		objectSave( byte **pBuffer ) {	
+		*(*pBuffer)++ = mState;
+
+		size_t strSize = cObject::objectSave( pBuffer );
+		
+		*(*pBuffer)++ = 0; *(*pBuffer)++ = 0;
+		*(*pBuffer)++ = 0; *(*pBuffer)++ = 0;
+
+		return (strSize + 5);
+	}
+
+};
